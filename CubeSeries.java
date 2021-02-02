ad0f9ba63c168d9306a765a09b00a08ed92c3945
Pratikshakolhe <78258593+Pratikshakolhe@users.noreply.github.com>
2021-02-02 23:25:45 +0530

    Add files via upload

---
+++ CubeSeries.java
@@ -0,0 +1,24 @@
+package day1;
+
+import java.util.Scanner;
+
+public class CubeSeries {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Number :");
+		int num=sc.nextInt();
+		for(int i=1;i<=num;i++)
+		{
+			int cube;
+			cube=i*i*i;
+			System.out.print(cube);
+			System.out.print("\t");
+			
+		}
+		
+
+	}
+
+}
